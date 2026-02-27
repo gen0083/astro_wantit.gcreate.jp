d464591dbbdd4aae13098996566e690dceb54d2e
Gen <archiherewego@gmail.com>
2026-02-27 19:35:04 +0900

    add: tailwind cssを導入（使えるように初期設定だけした）
    npx astro add tailwindで導入ができる

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		server: {
-			watch: {
-				// baseで指定したフォルダ（例: content）を監視対象に含める
-				// これにより、フォルダ内の画像やMarkdownの変更が検知されやすくなります
-				ignored: ['!**/content/**'],
-			},
-		},
-	},
+    vite: {
+      server: {
+          watch: {
+              // baseで指定したフォルダ（例: content）を監視対象に含める
+              // これにより、フォルダ内の画像やMarkdownの変更が検知されやすくなります
+              ignored: ['!**/content/**'],
+          },
+      },
+      plugins: [tailwindcss()],
+    },
 });
