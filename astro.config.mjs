3a2e6281386434c965c51647463d1df1b7aa4c10
Gen <archiherewego@gmail.com>
2026-03-11 13:40:46 +0900

    config: URLとtrailingSlashの設定を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://wantit.gcreate.jp',
+  trailingSlash: 'always',
   vite: {
     server: {
       watch: {
