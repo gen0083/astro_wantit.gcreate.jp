53f9b028106c8a2e480ce846eafe039e80431a92
Gen <archiherewego@gmail.com>
2026-03-30 19:27:49 +0900

    feature: 記事への修正リクエストのためのリンクを追加

---
+++ src/util/github.ts
@@ -0,0 +1,16 @@
+import type { CollectionEntry } from "astro:content";
+
+/**
+ * 開発中はvscodeで該当ファイルを開くリンクになり、本番ではgithubの該当ファイルへのリンクURLを返す
+ * @param post ファイルパスを知りたいpost
+ * @returns 該当ファイルへのリンク（devで動かしているときはvscodeで開く、そうでない場合はgithubへのリンク）
+ */
+export const getPostFilePath = (post: CollectionEntry<"post">) => {
+	const filePath = post.filePath ?? "";
+
+	if (import.meta.env.DEV) {
+		return `vscode://file${process.cwd()}/${filePath}`;
+	}
+
+	return `https:/github.com/gen0083/astro_wantit/blob/main/${filePath}`;
+};
