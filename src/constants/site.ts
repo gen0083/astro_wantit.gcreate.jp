c5f4116a4ba3f5fded61b98f08d8ee3324a0d3b7
Gen <archiherewego@gmail.com>
2026-04-08 14:26:59 +0900

    feature: サイト説明文を設定

---
+++ src/constants/site.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = "イイモノ";
+export const SITE_DESCRIPTION = "パソコン、PC周辺機器、カメラ、自転車、コーヒー、本、ライフハック術など、私が日々生活する中で買ったもの、使ってみたものを記録し紹介するブログです。興味をもった理由を絡めて、実際の使い心地をレポートしていきます。";
+export const AUTHOR = "gen0083";
