31fab8ef0ccaf92ec065e203f60815509e2de1a1
Gen <archiherewego@gmail.com>
2026-04-20 14:00:16 +0900

    feature: 不要なブランチを掃除するスクリプトを追加

---
+++ scripts/delete-merged-branch.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# refer: https://gist.github.com/miukoba/fc3c10a25c1c675c1e97
+
+git fetch origin main:main && \
+git checkout main && \
+git branch --merged main | grep -vE '^\*|\<main\>' | xargs -I % git branch -d % && \
+git branch -r --merged main | grep -vE '\<main\>|HEAD' | sed -e 's% *origin/%%' | xargs -I% git push --delete origin % && \
+git fetch --prune
